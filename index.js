29f45629eeae85156ed450883a6292cd2230d728
Joe Corcoran <joecorcoran@gmail.com>
2013-12-07 15:34:04 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var config  = require('./config.json'),
+    conn    = require('midi-launchpad').connect(0),
+
+    Telepad = require('./lib/telepad').Telepad,
+    server  = require('./lib/server').server;
+
+
+var telepad;
+
+conn.on('ready', function(launchpad) {
+  telepad = new Telepad(launchpad, config.numbers);
+  server.listen(9999);
+  console.log('Hello');
+  launchpad.on('press', function(button) {
+    if (button.x === 8) {
+      telepad.callNumber(button.y);
+    } else {
+      telepad.playSound(button.x, button.y);
+    }
+  });
+});
+
+process.on('SIGINT', function() {
+  telepad.launchpad.clear();
+  console.log('\nGoodbye');
+  process.exit();
+});
